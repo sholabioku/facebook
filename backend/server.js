83777a1bbf3d601c7252c049847a6df6aecd8027
sholabioku <bilushonline@gmail.com>
2022-09-10 18:12:24 +0100

    Add mongodb database setup functionlity

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const colors = require('colors');
 const cors = require('cors');
@@ -12,6 +13,14 @@ app.use(cors());
 readdirSync('./routes').map((route) =>
   app.use('/', require(`./routes/${route}`))
 );
+
+const connectDB = async () => {
+  const conn = await mongoose.connect(process.env.DATABASE_URL);
+
+  console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline.bold);
+};
+
+connectDB();
 
 const PORT = process.env.PORT || 8000;
 
