f58cf0dead9cfee8f850d4d3e2f89050b684d66a
sholabioku <bilushonline@gmail.com>
2022-09-10 18:16:08 +0100

    Refracture codes

--- backend/server.js
+++ backend/server.js
@@ -4,23 +4,17 @@ const dotenv = require('dotenv');
 const colors = require('colors');
 const cors = require('cors');
 const { readdirSync } = require('fs');
+const connectDB = require('./config/db');
 
 const app = express();
 dotenv.config();
+connectDB();
 
 app.use(cors());
 
 readdirSync('./routes').map((route) =>
   app.use('/', require(`./routes/${route}`))
 );
-
-const connectDB = async () => {
-  const conn = await mongoose.connect(process.env.DATABASE_URL);
-
-  console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline.bold);
-};
-
-connectDB();
 
 const PORT = process.env.PORT || 8000;
 
