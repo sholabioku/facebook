872c34c17e813b708e9a05d8ffce100ca2328470
sholabioku <bilushonline@gmail.com>
2022-09-10 17:34:26 +0100

    Install cors package and use it

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const colors = require('colors');
+const cors = require('cors');
 const app = express();
 
+app.use(cors());
 dotenv.config();
 const PORT = process.env.PORT || 8000;
 
