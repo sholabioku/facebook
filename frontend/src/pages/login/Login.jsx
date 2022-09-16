0043997d43d36b92cde6ff4e57ea5e306c4e08ea
sholabioku <bilushonline@gmail.com>
2022-09-16 11:39:23 +0100

    Add responsiveness for 850 and above

--- frontend/src/pages/login/Login.jsx
+++ frontend/src/pages/login/Login.jsx
@@ -36,7 +36,7 @@ const Login = () => {
           <div className='login_1'>
             <img src='../../icons/facebook.svg' alt='' />
             <span>
-              Facebook helps you connect and share with the people in your life
+              Facebook helps you connect and share with the people in your life.
             </span>
           </div>
           <div className='login_2'>
@@ -71,14 +71,13 @@ const Login = () => {
                 )}
               </Formik>
               <Link to='/forgot' className='forgot_password'>
-                Forgotten password ?
+                Forgotten password?
               </Link>
               <div className='sign_splitter'></div>
               <button className='blue_btn open_signup'>Create Account</button>
             </div>
             <Link to='/' className='sign_extra'>
-              <b>Create a Page</b>
-              for a celebrity, brand or business
+              <b>Create a Page</b> for a celebrity, brand or business
             </Link>
           </div>
         </div>
