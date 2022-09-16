554fb56c35571d52f275590c6eedee8adf6a7f53
sholabioku <bilushonline@gmail.com>
2022-09-16 08:02:13 +0100

    Add functionality for error icon in the input

--- frontend/src/components/inputs/loginInput/LoginInput.jsx
+++ frontend/src/components/inputs/loginInput/LoginInput.jsx
@@ -10,13 +10,14 @@ const LoginInput = ({ placeholder, ...props }) => {
         {meta.touched && meta.error && <ErrorMessage name={field.name} />}
       </div>
       <input
-        className={meta.touched && meta.error && 'input_error_border'}
+        className={meta.touched && meta.error ? 'input_error_border' : ''}
         type={field.type}
         name={field.name}
         placeholder={placeholder}
         {...field}
         {...props}
       />
+      {meta.touched && meta.error && <i className='error_icon' />}
     </div>
   );
 };
