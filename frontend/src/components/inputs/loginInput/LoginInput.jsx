b383dcc36d37a35059671fc897d680c734b5f614
sholabioku <bilushonline@gmail.com>
2022-09-16 12:16:37 +0100

    Add responsiveness for error background

--- frontend/src/components/inputs/loginInput/LoginInput.jsx
+++ frontend/src/components/inputs/loginInput/LoginInput.jsx
@@ -1,16 +1,29 @@
 import { ErrorMessage, useField } from 'formik';
+import { useMediaQuery } from 'react-responsive';
+
 import './loginInput.css';
 
 const LoginInput = ({ placeholder, bottom, ...props }) => {
   const [field, meta] = useField(props);
 
+  const desktopView = useMediaQuery({
+    query: '(min-width: 850px)',
+  });
+
   return (
     <div className='input_wrap'>
       {meta.touched && meta.error && !bottom && (
-        <div className='input_error' style={{ transform: 'translateY(3px)' }}>
+        <div
+          className={
+            desktopView ? 'input_error input_error_desktop' : 'input_error'
+          }
+          style={{ transform: 'translateY(3px)' }}
+        >
           {meta.touched && meta.error && <ErrorMessage name={field.name} />}
           {meta.touched && meta.error && (
-            <div className='error_arrow_top'></div>
+            <div
+              className={desktopView ? 'error_arrow_left' : 'error_arrow_top'}
+            ></div>
           )}
         </div>
       )}
@@ -24,16 +37,28 @@ const LoginInput = ({ placeholder, bottom, ...props }) => {
       />
 
       {meta.touched && meta.error && bottom && (
-        <div className='input_error' style={{ transform: 'translateY(1px)' }}>
+        <div
+          className={
+            desktopView ? 'input_error input_error_desktop' : 'input_error'
+          }
+          style={{ transform: 'translateY(1px)' }}
+        >
           {meta.touched && meta.error && <ErrorMessage name={field.name} />}
           {meta.touched && meta.error && (
-            <div className='error_arrow_bottom'></div>
+            <div
+              className={
+                desktopView ? 'error_arrow_left' : 'error_arrow_bottom'
+              }
+            ></div>
           )}
         </div>
       )}
 
       {meta.touched && meta.error && (
-        <i className='error_icon' style={{ top: `${!bottom && '63%'}` }} />
+        <i
+          className='error_icon'
+          style={{ top: `${!bottom && !desktopView ? '63%' : '15px'}` }}
+        />
       )}
     </div>
   );
