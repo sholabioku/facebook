7d0fcb63856eb08a3ab533439f92e9ad526b0860
sholabioku <bilushonline@gmail.com>
2022-09-16 07:52:43 +0100

    Add functionality for error border-color for input

--- frontend/src/components/inputs/loginInput/LoginInput.jsx
+++ frontend/src/components/inputs/loginInput/LoginInput.jsx
@@ -10,6 +10,7 @@ const LoginInput = ({ placeholder, ...props }) => {
         {meta.touched && meta.error && <ErrorMessage name={field.name} />}
       </div>
       <input
+        className={meta.touched && meta.error && 'input_error_border'}
         type={field.type}
         name={field.name}
         placeholder={placeholder}
