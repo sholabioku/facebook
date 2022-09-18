7ab86eca1dc8dc43df765e97d6e9199955d67ca4
sholabioku <bilushonline@gmail.com>
2022-09-18 07:29:00 +0100

    Add functionality to manipulate input at small screen pf 539

--- frontend/src/components/inputs/registerInput/RegisterInput.jsx
+++ frontend/src/components/inputs/registerInput/RegisterInput.jsx
@@ -6,14 +6,31 @@ import './registerInput.css';
 const RegisterInput = ({ placeholder, bottom, ...props }) => {
   const [field, meta] = useField(props);
 
-  const desktopView = useMediaQuery({
+  const view1 = useMediaQuery({
+    query: '(min-width: 539px)',
+  });
+
+  const view2 = useMediaQuery({
     query: '(min-width: 850px)',
+  });
+
+  const view3 = useMediaQuery({
+    query: '(min-width: 1170px)',
   });
 
   return (
     <div className='input_wrap register_input_wrap'>
       <input
         className={meta.touched && meta.error ? 'input_error_border' : ''}
+        style={{
+          width: `${
+            view1 && (field.name === 'first_name' || field.name === 'last_name')
+              ? '100%'
+              : view1 && (field.name === 'email' || field.name === 'password')
+              ? '370px'
+              : '300px'
+          }`,
+        }}
         type={field.type}
         name={field.name}
         placeholder={placeholder}
@@ -23,17 +40,13 @@ const RegisterInput = ({ placeholder, bottom, ...props }) => {
 
       {meta.touched && meta.error && (
         <div
-          className={
-            desktopView ? 'input_error input_error_desktop' : 'input_error'
-          }
+          className={view2 ? 'input_error input_error_desktop' : 'input_error'}
           style={{ transform: 'translateY(1px)' }}
         >
           {meta.touched && meta.error && <ErrorMessage name={field.name} />}
           {meta.touched && meta.error && (
             <div
-              className={
-                desktopView ? 'error_arrow_left' : 'error_arrow_bottom'
-              }
+              className={view2 ? 'error_arrow_left' : 'error_arrow_bottom'}
             ></div>
           )}
         </div>
