896b9f2e94d2392ffc80cb40c43e660ca09710b3
sholabioku <bilushonline@gmail.com>
2022-09-20 14:38:54 +0100

    Fix

--- frontend/src/components/login/RegisterForm.jsx
+++ frontend/src/components/login/RegisterForm.jsx
@@ -177,7 +177,7 @@ const RegisterForm = () => {
               <div className='reg_infos'>
                 By clicking Sign Up, you agree to our{' '}
                 <span>Terms, Data Policy &nbsp;</span> and
-                <span>Cookie Policy.</span>You may receive SMS notifications
+                <span> Cookie Policy.</span>You may receive SMS notifications
                 from us and can opt out at any time.
               </div>
               <div className='reg_btn_wrapper'>
