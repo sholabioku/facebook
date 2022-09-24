60915b28b7e4da439c36eeb052f71824eee7b236
sholabioku <bilushonline@gmail.com>
2022-09-24 09:45:41 +0100

    Add header and jsx-css-styling for the left part of the header

---
+++ frontend/src/components/header/Header.jsx
@@ -0,0 +1,32 @@
+import { Link } from 'react-router-dom';
+import { Logo, Search } from '../../svg';
+
+import './header.css';
+
+const Header = () => {
+  const color = '#65676b';
+
+  return (
+    <header>
+      <div className='header_left'>
+        <Link to='/' className='header_logo'>
+          <div className='circle'>
+            <Logo />
+          </div>
+        </Link>
+        <div className='search search1'>
+          <Search color={color} />
+          <input
+            type='text'
+            placeholder='Search Facebook'
+            className='hide_input'
+          />
+        </div>
+      </div>
+      <div className='header_middle'></div>
+      <div className='header_right'></div>
+    </header>
+  );
+};
+
+export default Header;
--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -1,7 +1,12 @@
+import Header from '../../components/header/Header';
 import './home.css';
 
 const Home = () => {
-  return <div>Home page</div>;
+  return (
+    <div>
+      <Header />
+    </div>
+  );
 };
 
 export default Home;
