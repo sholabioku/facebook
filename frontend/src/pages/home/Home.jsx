82ffd86421d46fa84feff87e7dbb263fd83b7f73
sholabioku <bilushonline@gmail.com>
2022-09-24 20:50:22 +0100

    Recovering from lost objects

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -1,10 +1,19 @@
+import { useRef, useState } from 'react';
 import Header from '../../components/header/Header';
+import useClickOutside from '../../helpers/clickOutside';
 import './home.css';
 
 const Home = () => {
+  const element = useRef(null);
+  const [visible, setVisible] = useState(true);
+  useClickOutside(element, () => {
+    setVisible(false);
+  });
+
   return (
     <div>
       <Header />
+      {visible && <div className='card' ref={element}></div>}
     </div>
   );
 };
