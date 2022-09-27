3b58e9fa749db275985fd3febce3220b2a0dd3af
sholabioku <bilushonline@gmail.com>
2022-09-27 09:09:38 +0100

    Add functionality to navigate help & support menu

---
+++ frontend/src/components/header/userMenu/HelpSupport.jsx
@@ -0,0 +1,32 @@
+const HelpSupport = ({ setVisible }) => {
+  return (
+    <div className='absolute_wrap'>
+      <div className='absolute_wrap_header'>
+        <div className='circle hover1' onClick={() => setVisible(0)}>
+          <i className='arrow_back_icon' />
+        </div>
+        Help & Support
+      </div>
+      <div className='menu_item hover3'>
+        <div className='small_circle'>
+          <i className='help_center_icon' />
+        </div>
+        <span>Help Center</span>
+      </div>
+      <div className='menu_item hover3'>
+        <div className='small_circle'>
+          <i className='email_icon' />
+        </div>
+        <span>Support Inbox</span>
+      </div>
+      <div className='menu_item hover3'>
+        <div className='small_circle'>
+          <i className='info_filled_icon' />
+        </div>
+        <span>Report a Problem</span>
+      </div>
+    </div>
+  );
+};
+
+export default HelpSupport;
