851b9eb23f40a0c058c62bb4699099aa1f756fc0
sholabioku <bilushonline@gmail.com>
2022-09-29 14:19:11 +0100

    Add functionality for protected routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,14 +2,20 @@ import { Routes, Route } from 'react-router-dom';
 import Home from './pages/home/Home';
 import Login from './pages/login/Login';
 import Profile from './pages/profile/Profile';
+import LoggedInRoutes from './routes/LoggedInRoutes';
+import NotLoggedInRoutes from './routes/NotLoggedInRoutes';
 
 const App = () => {
   return (
     <div>
       <Routes>
-        <Route path='/' element={<Home />} exact />
-        <Route path='/login' element={<Login />} exact />
-        <Route path='/profile' element={<Profile />} exact />
+        <Route element={<LoggedInRoutes />}>
+          <Route path='/' element={<Home />} exact />
+          <Route path='/profile' element={<Profile />} exact />
+        </Route>
+        <Route element={<NotLoggedInRoutes />}>
+          <Route path='/login' element={<Login />} exact />
+        </Route>
       </Routes>
     </div>
   );
---
+++ frontend/src/routes/NotLoggedInRoutes.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { useSelector } from 'react-redux';
+import { Navigate, Outlet } from 'react-router-dom';
+
+const NotLoggedInRoutes = () => {
+  const { user } = useSelector((state) => ({ ...state }));
+
+  return user ? <Navigate to='/' /> : <Outlet />;
+};
+
+export default NotLoggedInRoutes;
