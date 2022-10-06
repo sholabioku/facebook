6b6c3e3dd0f6d9fb5440345ac1b0a018312211c5
sholabioku <bilushonline@gmail.com>
2022-10-06 17:32:28 +0100

    Add jsx-css-styling for stories home

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -2,6 +2,7 @@ import { useRef, useState } from 'react';
 import { useSelector } from 'react-redux';
 import Header from '../../components/header/Header';
 import LeftHome from '../../components/home/leftHome/LeftHome';
+import Stories from '../../components/home/stories/Stories';
 
 import './home.css';
 import RightHome from './rightHome/RightHome';
@@ -10,9 +11,12 @@ const Home = () => {
   const { user } = useSelector((user) => ({ ...user }));
 
   return (
-    <div>
+    <div className='home'>
       <Header />
       <LeftHome user={user} />
+      <div className='home_middle'>
+        <Stories />
+      </div>
       <RightHome user={user} />
     </div>
   );
