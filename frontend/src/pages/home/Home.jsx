84474fc8ec335a8e10f94f435aecc6a92e8bc9c2
sholabioku <bilushonline@gmail.com>
2022-10-07 08:56:36 +0100

    Add jsx-css-styling create post form

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -1,5 +1,6 @@
 import { useRef, useState } from 'react';
 import { useSelector } from 'react-redux';
+import CreatePost from '../../components/createPost/CreatePost';
 import Header from '../../components/header/Header';
 import LeftHome from '../../components/home/leftHome/LeftHome';
 import Stories from '../../components/home/stories/Stories';
@@ -16,6 +17,7 @@ const Home = () => {
       <LeftHome user={user} />
       <div className='home_middle'>
         <Stories />
+        <CreatePost user={user} />
       </div>
       <RightHome user={user} />
     </div>
