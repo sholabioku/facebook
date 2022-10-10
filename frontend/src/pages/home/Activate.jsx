bfca9337a7ae149dff0858b7ce4bb6c3a2126356
sholabioku <bilushonline@gmail.com>
2022-10-10 05:02:24 +0100

    Add jsx-css-styling popup message for account verification

--- frontend/src/pages/home/Activate.jsx
+++ frontend/src/pages/home/Activate.jsx
@@ -5,16 +5,33 @@ import Header from '../../components/header/Header';
 import LeftHome from '../../components/home/leftHome/LeftHome';
 import Stories from '../../components/home/stories/Stories';
 import RightHome from '../../components/home/rightHome/RightHome';
+import ActivateForm from './ActivateForm';
 
 const Activate = () => {
   const { user } = useSelector((user) => ({ ...user }));
 
   const [success, setSuccess] = useState('');
   const [error, setError] = useState('');
-  const [loading, setLoading] = useState(false);
+  const [loading, setLoading] = useState(true);
 
   return (
     <div className='home'>
+      {success && (
+        <ActivateForm
+          type='success'
+          header='Account verification succeeded'
+          text={success}
+          loading={loading}
+        />
+      )}
+      {error && (
+        <ActivateForm
+          type='error'
+          header='Account verification failed'
+          text={error}
+          loading={loading}
+        />
+      )}
       <Header />
       <LeftHome user={user} />
       <div className='home_middle'>
