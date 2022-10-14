244402a4323ccd053753f8e86040777b2463a39b
sholabioku <bilushonline@gmail.com>
2022-10-14 08:38:05 +0100

    Add jsx-css-styling reset page header and functionality

--- frontend/src/components/login/LoginForm.jsx
+++ frontend/src/components/login/LoginForm.jsx
@@ -104,7 +104,7 @@ const LoginForm = ({ setVisible }) => {
               </Form>
             )}
           </Formik>
-          <Link to='/forgot' className='forgot_password'>
+          <Link to='/reset' className='forgot_password'>
             Forgotten password?
           </Link>
           <DotLoader color='#1876f2' loading={loading} size={30} />
--- frontend/src/pages/reset/Reset.jsx
+++ frontend/src/pages/reset/Reset.jsx
@@ -1,7 +1,41 @@
+import { Link, useNavigate } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import Cookies from 'js-cookie';
+
 import './reset.css';
 
 const Reset = () => {
-  return <div>Reset</div>;
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const { user } = useSelector((state) => ({ ...state }));
+
+  const logout = () => {
+    Cookies.set('user', '');
+    dispatch({ type: 'LOGOUT' });
+    navigate('/login');
+  };
+
+  return (
+    <div className='reset'>
+      <div className='reset_header'>
+        <img src='../../../icons/facebook.svg' alt='' />
+        {user ? (
+          <div className='right_reset'>
+            <Link to='/profile'>
+              <img src={user.picture} alt='' />
+            </Link>
+            <button className='blue_btn' onClick={() => logout()}>
+              Logout
+            </button>
+          </div>
+        ) : (
+          <Link to='/login' className='right_reset'>
+            <button className='blue_btn'>Login</button>
+          </Link>
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default Reset;
